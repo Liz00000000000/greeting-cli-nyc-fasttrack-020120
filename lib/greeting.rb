56f14a2fe1a350324288441fe6da84fd6fc8c971
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 01:01:12 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,8 @@
 # code the #greeting method here!
+require_relative "../lib/greeting.rb"
+def greeting ("elizabeth")
+puts "Hi! I'm HAL, whats your name?"
+puts "Hello #{name.strip}. It's nice to meet you."
+end
+
+greeting(elizabeth)
