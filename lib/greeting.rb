35b93217e54dcd8fafb087a3327f9e7991662622
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 01:14:04 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
-def greeting(name = elizabeth)
+def greeting(name = Don)
 puts "Hello #{name.strip}. It's nice to meet you."
 end
 
-greeting("elizabeth")
+greeting("Don")
